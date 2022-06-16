2eab1059beeb1d0b558f32a0848580154e6e71a8
Kirill Voronchihin <skiletoons@mail.ru>
2022-06-16 14:27:24 +0700

    Задача 60 Атрибуты в классе Tag

--- tag.php
+++ tag.php
@@ -1,23 +1,39 @@
-<?php
-class Tag
+	class Tag
 	{
 		private $name;
 
+		
 		public function __construct($name)
 		{
 			$this->name = $name;
 		}
 
+		
 		public function open()
 		{
 			$name = $this->name;
 			return "<$name>";
 		}
 
+		
 		public function close()
 		{
 			$name = $this->name;
 			return "</$name>";
 		}
+		private function getAttrsStr($attrs)
+	{
+		if (!empty($attrs)) {
+			$result = '';
+
+			foreach ($attrs as $name => $value) {
+				$result .= " $name=\"$value\"";
+			}
+
+			return $result;
+		} else {
+			return '';
+		}
+	}
 	}
 ?>
