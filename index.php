38931ed16e15874b3d6324c3ed73dccbb53092d3
Kirill Voronchihin <skiletoons@mail.ru>
2022-06-16 21:52:56 +0700

    Задача 74 класс password

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
 <?php
-	require_once 'Form.php';
-	require_once 'Input.php';
-	require_once 'Submit.php';
+	require_once 'form.php';
+	require_once 'input.php';
+	require_once 'submit.php';
+	require_once 'pasword.php';
 
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
-	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	echo $form->open();
-		echo (new Input())->setAttrs(['name'=>'num1', 'value' => '1']);
-		echo (new Input())->setAttrs(['name'=>'num2', 'value' => '2']);
-		echo (new Input())->setAttrs(['name'=>'num3', 'value' => '3']);
-		echo (new Input())->setAttrs(['name'=>'num4', 'value' => '4']);
-		echo (new Input())->setAttrs(['name'=>'num5', 'value' => '5']);
-		echo (new Input())->setAttrs(['type'=>'submit']);
 		echo (new Input)->setAttr('name', 'year');
+		echo (new Input)   ->setAttr('name', 'login');
+		echo (new Pswrd)->setAttr('name', 'passw');
 		echo new Submit;
 	echo $form->close();
-
-	if(isset($_GET['num1'])){
-	echo $sum = $_GET['num1'] + $_GET['num2'] + $_GET['num3'] + $_GET['num4'] + $_GET['num5'];
-	}
-?> 
+?>
