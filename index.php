fe651d5910c5a820f18f4c530a82239ada16e860
Kirill Voronchihin <skiletoons@mail.ru>
2022-06-16 22:02:28 +0700

    Задача 77 класс checkbox

--- index.php
+++ index.php
@@ -4,14 +4,16 @@
 	require_once 'Submit.php';
 	require_once 'pas.php';
 	require_once 'Hidden.php';
+	require_once 'Textarea.php';
+	require_once 'Checkbox.php';
 
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
+	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 
 	echo $form->open();
-		echo (new Input)   ->setAttr('name', 'login');
-		echo (new pas)->setAttr('name', 'password');
-		echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123');
-		echo (new Input)->setAttr('name', 'year');
+		echo (new Checkbox)->setAttr('name', 'checkbox');
+		echo (new Input)->setAttr('name', 'user');
+		echo (new Textarea)->setAttr('name', 'message')->show();
 		echo new Submit;
 	echo $form->close();
 ?> 
