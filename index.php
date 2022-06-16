608ccb0461deb3438dd546ba0828445528baaa0b
Kirill Voronchihin <skiletoons@mail.ru>
2022-06-16 14:13:54 +0700

    Задача 59 класс Tag

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php 
-require_once 'date.php';
-require_once 'interval.php';
 require_once 'iFile.php';
 require_once 'File.php';
+require_once 'tag.php';
 
-	$date1 = new Date('2025-12-31');
-	$date2 = new Date('2026-11-28');
-	$file = new File('index.php');
+$file = new File('index.php');
 
-	$interval = new Interval($date1, $date2);
 echo $file->getPath().'<br>';
 echo $file->getDir().'<br>';
 echo $file->getExt().'<br>';
@@ -16,10 +12,9 @@ echo $file->getName().'<br>';
 echo $file->getSize().' байт'.'<br>';
 
 echo $file->getText();
+	$img = new Tag('img');
+	echo $img->open().''.$img->close(); 
 
-	echo $interval->toDays();   // выведет разницу в днях
-	echo '<br>';
-	echo $interval->toMonths(); // выведет разницу в месяцах
-	echo '<br>';
-	echo $interval->toYears();  // выведет разницу в годах
-?>
+	$header = new Tag('header');
+	echo $header->open().'header сайта'.$header->close();
+?> 
