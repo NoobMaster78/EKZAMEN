956f5eadfbec7f5d618c1d5af57711aca0d73d6c
Kirill Voronchihin <skiletoons@mail.ru>
2022-06-16 22:07:19 +0700

    Задача на JS

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
-<?php
-	require_once 'Form.php';
-	require_once 'Input.php';
-	require_once 'Submit.php';
-	require_once 'pas.php';
-	require_once 'Hidden.php';
-	require_once 'Textarea.php';
-	require_once 'Checkbox.php';
-
-	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
-	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
-
-	echo $form->open();
-		echo (new Checkbox)->setAttr('name', 'checkbox');
-		echo (new Input)->setAttr('name', 'user');
-		echo (new Textarea)->setAttr('name', 'message')->show();
-		echo new Submit;
-	echo $form->close();
-?> 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Задача 8</title>
+    <script src="JAVA.js"></script> 
+  </head>
+  <body>
+<input type="text" value="Нажми меня!" onfocus="btn()">
+  </body>
+</html>
